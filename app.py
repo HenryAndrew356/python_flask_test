b68905e9fd035f8669a5b28cffea7eec9dbc2334
HenryAndrew356 <58161132+HenryAndrew356@users.noreply.github.com>
2023-05-29 21:04:05 -0500

    commit commit

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SESSION_TYPE"]="filesystem"
 Session(app)
 
 
-
+#   Pagina Inicial donde podemos dirigirnos a las demas
 
 @app.route('/')
 def init():
@@ -30,6 +30,8 @@ def hello():
     name_=  request.form.get("name")
     return render_template("hello.html",name=name_)
 
+#   Sin categoria
+
 
 @app.route('/about')
 def about():
@@ -48,10 +50,25 @@ def page_02_inh():
     return render_template("page_02_inh.html")
 
 
+#Looops
+
 
 @app.route('/loop_page')
 def def_loop_field():
-    name_list_=["n_f_01","n_f_02","n_f_03"]
+    name_list_=[
+        "n_f_01",
+        "n_f_02",
+        "n_f_03",
+        "n_f_04",
+        "n_f_05",
+        "n_f_06",
+        "n_f_0 siete",
+        "n_f_0 ocho",
+        "n_f_0 nueve",
+        "n_f_0 diez",
+        "n_f_0 once",
+        "n_f_0 once",
+        ]
     return render_template("loop.html",names_list=name_list_)
 
 
@@ -66,7 +83,7 @@ def route_datetime():
 
 
 #       MACROS
-
+#           no entiendo
 
 notes=[]
 
@@ -92,17 +109,46 @@ def route_notes():
     return render_template("index_notes.html",notes_02_field=notes_02)
 
 
-#       ROUTES <-- DENTRO DE TODAS
+#       ROUTES0
+#           el enrutamiento se usa en todo
+
+#       ROUTES1
+
+
+@app.route("/route_01/<string:text_message>")
+def route_01_dinamic(text_message):
+    mensaje_dinamico=text_message;
+    return render_template("routes_01.html",mensage=mensaje_dinamico)
+    
+
 
 #       URLS <-- cuando declaro el url_for en todas
 # 
 #       TEMPLATES <-- usado en todas los enrutamibne y herenia de la plantilla base
 #       VARIABBLES <-- creada dento del decorator
+
+
+
+
+#       VARIABLES0
+#           pasa un valor como parametro en el render_template
+
+
+
 #       VARIABLES_01
 
 @app.route("/var_01")
-def route_01():
-    headline_=random.choice(["Hello, World","Hi. there", "Good Morning"])
+def route_var_01():
+    headline_=random.choice([
+        "Hello, World",
+        "Hi. there",
+        "Good Morning",
+        "message 000",
+        "message 002",
+        "message 003",
+        "message 004",
+        "message 005"
+        ])
     return render_template("var_01.html",headline_field=headline_)
 
 
